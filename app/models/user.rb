7575eb07a6b799631b33d3e5975e5749b7996375
Martin Gyhagen <martin.gyhagen@gmail.com>
2014-01-10 23:46:34 +0100

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
 	has_many  :microposts, dependent: :destroy
+	
+	has_many	:relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many 	:followed_users, through: :relationships, source: :followed
+	
+	has_many :reverse_relationships, foreign_key: "followed_id", 
+																	class_name: "Relationship", 
+																	dependent: :destroy
+	has_many :followers, through: :reverse_relationships, source: :follower
+
 	has_secure_password
 	
 	before_save { email.downcase! }
@@ -10,7 +19,6 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, 
 					  format: { with: VALID_EMAIL_REGEX }, 
 					  uniqueness: { case_sensitive: false }
-	has_secure_password
 	validates :password, length: { minimum: 6 }
 
 	def User.new_remember_token
@@ -23,7 +31,21 @@ class User < ActiveRecord::Base
 
 	def feed
 		# This is preliminary. See "Following users" for the full implementation.
-		Micropost.where("user_id = ?", id)
+		#Micropost.where("user_id = ?", id)
+
+		Micropost.from_users_followed_by(self)
+	end
+
+	def following?(other_user)
+		relationships.find_by(followed_id: other_user.id)
+	end
+
+	def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+	def unfollow!(other_user)
+		relationships.find_by(followed_id: other_user.id).destroy!
 	end
 
 	private
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,6 +20,7 @@ describe "Static Pages" do
 
 		describe "for signed-in users" do
 			let(:user) { FactoryGirl.create(:user) }
+			
 			before do
 				FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
 				FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
@@ -31,6 +32,17 @@ describe "Static Pages" do
 				user.feed.each do |item|
 					expect(page).to have_selector("li##{item.id}", text: item.content)
 				end
+			end
+
+			describe "follower/following counts" do
+				let(:other_user) { FactoryGirl.create(:user) }
+				before do
+					other_user.follow!(user)
+					visit root_path
+				end
+
+				it { should have_link("0 following", href: following_user_path(user)) }
+				it { should have_link("1 follower", href: followers_user_path(user)) }
 			end
 		end
 	end
